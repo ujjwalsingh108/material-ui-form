a198323e2babce16a77e89a7168ccddcdf16d584
ujjwal-ampcome <ujjwal.singh@ampcome.com>
2024-12-12 11:08:03 +0530

    added the redirect route

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ function App() {
             <Route path={"leads"} element={<PageNotFound />} />
             <Route path="form-summary" element={<FormSubmission />} />
           </Route>
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
     </GlobalStateProvider>
